00539109000ac996e5ea3cc94167f07ed5c3e74e
gZanda <gabrielfzanda@gmail.com>
2023-07-05 01:57:57 -0300

    Leaflet Map With Icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import { Icon } from 'leaflet';
 
 function App() {
+
+  // Markers
+  const markers = [
+    {
+      geoCode: [-21.2335, -44.9984],
+      popUp: " Casinha "
+    },
+    {
+      geoCode: [-21.2275, -44.9815],
+      popUp: " UFLA "
+    }
+  ]
+
+  // Custom Icon
+  const myIcon = new Icon({
+    iconUrl:  require("./Icon.png"),
+    iconSize: [30, 30]
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <MapContainer center={[-21.2399, -44.9984]} zoom={14} >
+
+        <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {markers.map((marker => (
+          <Marker position={marker.geoCode} icon={myIcon} >
+            <Popup>
+              {marker.popUp}
+            </Popup>
+          </Marker>
+        )))}
+
+      </MapContainer>
+
     </div>
+
+    
+
   );
 }
 
